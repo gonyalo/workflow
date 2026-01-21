2911862588f4a16b9293f1c0181f808f2886a170
goncalo <joaogoncalo.borges@gmail.com>
2026-01-21 20:55:40 +0000

    first commit

---
+++ src/test/java/pt/goncaloborges/sandbox/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.goncaloborges.sandbox.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
