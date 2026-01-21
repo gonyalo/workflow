0b59f2415cb9b6ab4f495cfeb76561120505fbbe
goncalo <joaogoncalo.borges@gmail.com>
2026-01-21 22:03:53 +0000

    frontend calling the backend locally

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,19 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import axios from "axios";
+
+axios.get('http://localhost:8080/api/hello')
+    .then(function (response) {
+        console.log(response);
+        document.getElementById('result').innerHTML = response.data;
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+        console.log("always executed");
+    });
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -14,6 +27,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
+      <p id="result"></p>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
